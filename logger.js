33dd98314ea08a8c5a2e713d6144b7f379075871
Rapha-Y <raph.yuki@gmail.com>
2020-10-09 14:13:50 -0300

    created a basic server

--- logger.js
+++ logger.js
@@ -10,4 +10,12 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger;
+const logger = new Logger();
+
+logger.on('message', (data) => {
+    console.log('Data: ', data);
+});
+
+logger.log('Hello World');
+logger.log('Goodbye World');
+logger.log('Hi World');
